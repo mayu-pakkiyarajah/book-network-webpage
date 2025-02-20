9a2aad03243a3ecc1218d7e62384adb8deee99cb
Mayuravel Pakkiyarajah <mayuravelp.21@uom.lk>
2025-02-20 23:48:21 +0530

    feat(login)

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  authRequest: AuthRequest = { email : '', password: '' };
+  errorMsg: Array<string> = []; 
+
+}
